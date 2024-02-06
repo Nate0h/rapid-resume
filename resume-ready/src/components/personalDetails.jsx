e0b0f0b2b71292f4c45cda02752625f6468e00cb
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-02-06 11:52:26 -0500

    Added State to Personal Details section

--- resume-ready/src/components/personalDetails.jsx
+++ resume-ready/src/components/personalDetails.jsx
@@ -1,24 +1,88 @@
 import { useState } from "react";
 
 function PersonalDetails() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [address, setAddress] = useState("");
+
+  function handleFullNameChange(e) {
+    setFullName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleTelephoneChange(e) {
+    setTelephone(e.target.value);
+  }
+
+  function handleAddressChange(e) {
+    setAddress(e.target.value);
+  }
+
   return (
     <div>
       <h2>Personal Details</h2>
 
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <label htmlFor="fullname">Fullname</label>
-        <input id="fullname" name="fullname" type="text" />
+        <input
+          value={fullName}
+          onChange={handleFullNameChange}
+          id="fullname"
+          type="text"
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input
+          value={email}
+          onChange={handleEmailChange}
+          type="email"
+          id="email"
+        />
 
         <label htmlFor="telephone">Phone Number</label>
-        <input type="tel" id="telephone" name="telephone" />
+        <input
+          value={telephone}
+          onChange={handleTelephoneChange}
+          type="tel"
+          id="telephone"
+        />
 
         <label htmlFor="address">Address</label>
-        <input type="text" id="address" name="address" />
+        <input
+          value={address}
+          onChange={handleAddressChange}
+          type="text"
+          id="address"
+        />
+        <div>
+          {fullName} {email} {telephone} {address}
+        </div>
+        <button onClick={Display}>Click Me!</button>
       </form>
     </div>
+  );
+}
+
+function Display() {
+  const name = document.getElementById("fullname");
+  const email = document.getElementById("email");
+  const phoneNo = document.getElementById("telephone");
+  const address = document.getElementById("address");
+
+  alert(name.value);
+  return (
+    <>
+      <div>
+        <div>{name.value}</div>
+        <div>{email.value}</div>
+        <div>{phoneNo.value}</div>
+        <div>{address.value}</div>
+      </div>
+    </>
   );
 }
 
