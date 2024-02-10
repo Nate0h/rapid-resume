4aa1bdd6fbd8ab556cc60729a5e8fb53ed03bf2c
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-02-09 22:28:25 -0500

    Styled resume + imported personalDetails to resume

--- resume-ready/src/components/resume.jsx
+++ resume-ready/src/components/resume.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
+import "../styles/Resume.css";
+import email from "../assets/email.png";
+import phone from "../assets/phone.png";
+import address from "../assets/address.png";
 
 function Resume({ personalDetails }) {
   return (
-    <div>
-      <h2>Resume</h2>
-      <div>{personalDetails.fullName}</div>
-      <div>{personalDetails.email}</div>
-      <div>{personalDetails.phone}</div>
-      <div>{personalDetails.address}</div>
-      <div>{personalDetails.aboutMe}</div>
+    <div className="displayResume">
+      <div className="header">
+        <h1>{personalDetails.fullName}</h1>
+        <div className="contactDetails">
+          <div className="contactInfo">
+            <img src={email} alt="" />
+            <div>{personalDetails.email}</div>
+          </div>
+
+          <div className="contactInfo">
+            <img src={phone} alt="" />
+            <span>{personalDetails.phone}</span>
+          </div>
+          <div className="contactInfo">
+            <img src={address} alt="" />
+            <span>{personalDetails.address}</span>
+          </div>
+        </div>
+      </div>
+      <h2 className="sectionHeader">About Me</h2>
+      <p className="aboutMeText">{personalDetails.aboutMe}</p>
+
+      <h2 className="sectionHeader">Education</h2>
+      <h2 className="sectionHeader">Experience</h2>
+      <h2 className="sectionHeader">Skills and Technologies</h2>
     </div>
   );
 }
